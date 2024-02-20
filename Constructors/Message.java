67d1543df06165cf8b9d68c2f4c288b7301d8c9b
javsort <javierortega336@gmail.com>
2024-02-20 11:47:49 +0100

    Small message update

--- Constructors/Message.java
+++ Constructors/Message.java
@@ -2,13 +2,16 @@ package Constructors;
 
 import java.io.Serializable;
 import javax.crypto.SecretKey;
+import java.util.*;
 
 public class Message implements Serializable {
 
     private SecretKey key;
     private byte[] message;
+    private Date time;
 
     public Message(SecretKey key, byte[] encrypted){
+        this.time = new Date();
         this.key = key;
         this.message = encrypted;
     }
@@ -19,5 +22,9 @@ public class Message implements Serializable {
 
     public byte[] getMessage(){
         return this.message;
+    }
+
+    public Date getTime(){
+        return this.time;
     }
 }
