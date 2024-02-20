2f3afa339c8c75754763ae462291f5f2d1a4f161
javsort <javierortega336@gmail.com>
2024-02-20 01:21:23 +0100

    Javier branch test

--- Controller/ClientController.java
+++ Controller/ClientController.java
@@ -5,7 +5,7 @@ import Server.ServerFrame;
 
 public class ClientController {
     public static void main(String[] args) {
-        System.out.println("Initializing Client");
+        System.out.println("Initializing Client, branch Jasbolas");
 
         //ServerFrame server = new ServerFrame(5000);
         ClientFrame client = new ClientFrame("127.0.0.1", 5000);
